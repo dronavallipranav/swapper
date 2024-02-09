57901120a22c5d2b82ee09f1519df84eb1142159
Pranav Dronavalli <dronavallipranav@gmail.com>
2024-02-09 16:01:42 -0600

    gin routing, raven init

---
+++ backend/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"swapper/db"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ravendb/ravendb-go-client"
+)
+
+func main() {
+	documentStore, err := db.InitDocumentStore([]string{"http://live-test.ravendb.net"}, "swapper")
+	if err != nil {
+		log.Fatalf("Failed to initialize document store: %v", err)
+	}
+	defer documentStore.Close()
+
+	r := gin.Default()
+
+	setupRoutes(r, documentStore)
+
+	if err := r.Run(); err != nil {
+		log.Fatalf("Failed to run server: %v", err)
+	}
+}
+
+func setupRoutes(r *gin.Engine, store *ravendb.DocumentStore) {
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, world!",
+		})
+	})
+}
