4587d34b4dd272c03a3edca20a5e2157097eb4dc
davidteather <34144122+davidteather@users.noreply.github.com>
2024-02-09 16:15:30 -0600

    hi

--- website/src/pages/HomePage.tsx
+++ website/src/pages/HomePage.tsx
@@ -3,14 +3,13 @@ import { Item } from "../models/Item";
 import { fetchAllItems } from "../services/ItemService";
 
 function HomePage() {
-    const [items, setItems] = useState<Item[] | null>(null)
+  const [items, setItems] = useState<Item[] | null>(null);
 
-    useEffect(() => {
-        fetchAllItems().then((items) => {
-            setItems(items);
-        })
-    }, [])
-
+  useEffect(() => {
+    fetchAllItems().then((items) => {
+      setItems(items);
+    });
+  }, []);
 
   return (
     <div className="container mx-auto px-4 py-10">
@@ -31,28 +30,30 @@ function HomePage() {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {items && items.map((item) => (
-          <div key={item.id} className="card w-96 bg-base-100 shadow-xl">
-            <figure className="px-10 pt-10">
-              <img
-                src="https://placeimg.com/400/225/tech"
-                alt="Item"
-                className="rounded-xl"
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">{item.name}</h2>
-              <p>{item.description}</p>
-              <div className="card-actions">
-                <button className="btn btn-primary"
-                  onClick={() => {
-                    window.location.href = `/item/${item.id}`;
-                  }}
-                >View Item</button>
+        {items &&
+          items.map((item) => (
+            <div key={item.id} className="card w-72 bg-base-100 shadow-xl">
+              {item.imageUrl && (
+                <figure>
+                  <img src={item.imageUrl} alt="Item" className="rounded-xl" />
+                </figure>
+              )}
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">{item.name}</h2>
+                <p>{item.description}</p>
+                <div className="card-actions">
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => {
+                      window.location.href = `/item/${item.id}`;
+                    }}
+                  >
+                    View Item
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
