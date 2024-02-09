2fc0fbd1d8026b9b0710372d206fe9d849396eb2
davidteather <34144122+davidteather@users.noreply.github.com>
2024-02-09 16:10:36 -0600

    with image

--- website/src/pages/ItemPage.tsx
+++ website/src/pages/ItemPage.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { Item } from '../models/Item'; // Ensure this path matches your project structure
-import { fetchItemById } from '../services/ItemService'; // Ensure this path matches your project structure
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Item } from "../models/Item";
+import { fetchItemById } from "../services/ItemService";
 
 const ItemPage = () => {
   let { itemID } = useParams();
@@ -12,28 +12,34 @@ const ItemPage = () => {
     fetchItemById(itemID).then((fetchedItem) => {
       setItem(fetchedItem);
     });
-  }, [itemID]); // Adding itemID as a dependency for useEffect
+  }, [itemID]);
 
   if (!item) {
-    return <div className="flex justify-center items-center h-screen">Loading...</div>;
+    return (
+      <div className="flex justify-center items-center h-screen">
+        Loading...
+      </div>
+    );
   }
 
   return (
     <div className="container mx-auto px-4 py-10 mt-8">
       <div className="max-w-4xl mx-auto shadow-lg rounded-lg overflow-hidden bg-base-200">
-        <div className="bg-cover bg-center h-56 p-4" style={{ backgroundImage: `url(${item.imageUrl})` }}>
-          {
-            item.categories && (
-              <div className="flex justify-end">
-              <span className="badge badge-ghost badge-lg">Categories: {
-                item.categories.map((category, _) => (
-                  <span key={category} className="badge badge-ghost badge-lg">{category}</span>
-                ))
-              
-              }</span>
+        <img src={item.imageUrl} />
+
+        <div className="bg-cover bg-center p-4">
+          {item.categories && (
+            <div className="flex justify-end">
+              <span className="badge badge-ghost badge-lg">
+                Categories:{" "}
+                {item.categories.map((category, _) => (
+                  <span key={category} className="badge badge-ghost badge-lg">
+                    {category}
+                  </span>
+                ))}
+              </span>
             </div>
-            )
-          }
+          )}
         </div>
         <div className="p-4">
           <h1 className="text-3xl font-bold mb-2">{item.name}</h1>
--- website/src/services/ItemService.tsx
+++ website/src/services/ItemService.tsx
@@ -5,28 +5,26 @@ const items: Item[] = [
     id: "1",
     name: "Vintage Lamp",
     description: "A beautiful vintage lamp to light up your room.",
-    imageUrl: "https://placeimg.com/640/480/any",
+    imageUrl: "https://i.imgur.com/p2xbAbE.jpeg",
     createdByUserID: '1'
   },
   {
     id: "2",
     name: "Leather Sofa",
     description: "A comfortable leather sofa in excellent condition.",
-    imageUrl: "https://placeimg.com/640/480/any",
+    imageUrl: "https://i.imgur.com/p2xbAbE.jpeg",
     createdByUserID: '2',
   },
   {
     id: "3",
     name: "Wooden Dining Table",
     description: "A sturdy wooden dining table for family dinners.",
-    imageUrl: "https://placeimg.com/640/480/any",
+    imageUrl: "https://i.imgur.com/p2xbAbE.jpeg",
     createdByUserID: '3'
   },
-  // Add more items as needed
 ];
 
 export const fetchItemById = async (itemId: string) => {
-  // Simulate an API call with a delay
   await new Promise<Item>((resolve) => setTimeout(resolve, 50));
 
   const item = items.find((item) => item.id === itemId);
