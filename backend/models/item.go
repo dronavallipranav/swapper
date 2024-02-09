338b70bd2d4df142a52c23ab8e5845674284ff67
Pranav Dronavalli <dronavallipranav@gmail.com>
2024-02-09 16:15:53 -0600

    make models consistent

--- backend/models/item.go
+++ backend/models/item.go
@@ -1,9 +1,10 @@
 package models
 
 type Item struct {
-	ID          string `json:"id,omitempty"`
-	UserID      string `json:"userId"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Status      string `json:"status"`
+	ID          string   `json:"id,omitempty"`
+	UserID      string   `json:"userId"`
+	Title       string   `json:"title"`
+	Description string   `json:"description"`
+	Categories  []string `json:"categories"`
+	Status      string   `json:"status"`
 }
