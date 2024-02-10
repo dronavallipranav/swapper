f1427d60dfac3eed1a2e2cd343fd1ebd549dffff
Pranav Dronavalli <dronavallipranav@gmail.com>
2024-02-10 15:42:45 -0600

    programatically create swapper db on start

--- backend/db/ravendb.go
+++ backend/db/ravendb.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"log"
+
 	ravendb "github.com/ravendb/ravendb-go-client"
 )
 
@@ -13,6 +15,15 @@ func InitDocumentStore(urls []string, databaseName string) (*ravendb.DocumentSto
 			return nil, err
 		}
 		documentStore = store
+	}
+	dbRecord := &ravendb.DatabaseRecord{
+		DatabaseName: databaseName,
+	}
+
+	createDBOp := ravendb.NewCreateDatabaseOperation(dbRecord, 1)
+	err := documentStore.Maintenance().Server().Send(createDBOp)
+	if err != nil {
+		log.Fatalf("Failed to create database: %s", err)
 	}
 	return documentStore, nil
 }
