891b3a04eb2af0b6516bb1776ab0fb79030f7f37
Pranav Dronavalli <dronavallipranav@gmail.com>
2024-02-14 01:37:10 -0600

    add time sent field to messages

--- backend/models/message.go
+++ backend/models/message.go
@@ -1,7 +1,12 @@
 package models
 
+import (
+	"time"
+)
+
 type Message struct {
-	SenderID    string `json:"senderID" binding:"required"`
-	RecipientID string `json:"recipientID" binding:"required"`
-	Text        string `json:"text" binding:"required"`
+	SenderID    string    `json:"senderID" binding:"required"`
+	RecipientID string    `json:"recipientID" binding:"required"`
+	Text        string    `json:"text" binding:"required"`
+	SentAt      time.Time `json:"sentAt"`
 }
--- website/src/models/Message.ts
+++ website/src/models/Message.ts
@@ -2,4 +2,5 @@ export interface Message {
     senderID: string;
     recipientID: string;
     text: string;
+    sentAt: Date;
 }
