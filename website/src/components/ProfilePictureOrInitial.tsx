c1c419a4d5c2596d3747e154e5416a10ca1ece79
davidteather <34144122+davidteather@users.noreply.github.com>
2024-02-18 17:41:08 -0600

    more styling

--- website/src/components/ProfilePictureOrInitial.tsx
+++ website/src/components/ProfilePictureOrInitial.tsx
@@ -1,26 +1,12 @@
 import { User } from "../models/User";
 
 const ProfilePictureOrInitial: React.FC<{ user: User | null }> = ({ user }) => {
-  if (!user) {
-    return (
-      <div className="w-10 h-10 rounded-full flex items-center justify-center overflow-hidden bg-gray-200 text-xl font-semibold">
-        {"?"}
-      </div>
-    );
-  }
-
   return (
-    <div className="w-10 h-10 rounded-full flex items-center justify-center overflow-hidden bg-gray-200 text-xl font-semibold">
-      {user.profilePicture ? (
-        <img
-          src={user.profilePicture}
-          alt={`${user.name}'s Profile`}
-          className="w-full h-full object-cover"
-        />
-      ) : user.name ? (
-        user.name.charAt(0).toUpperCase()
+    <div className="w-10 h-10 rounded-full overflow-hidden bg-gray-300 text-xl font-semibold flex items-center justify-center">
+      {user?.profilePicture ? (
+        <img src={user.profilePicture} alt={`${user.name}'s Profile`} className="object-cover w-full h-full" />
       ) : (
-        "?"
+        <span className="text-white font-bold">{user?.name ? user.name.charAt(0).toUpperCase() : "?"}</span>
       )}
     </div>
   );
