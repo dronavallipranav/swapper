f9635fdea628d985cca323ba2304d16a1e8af26b
Pranav Dronavalli <dronavallipranav@gmail.com>
2024-02-20 12:39:48 -0600

    remove footer

--- website/src/pages/AboutPage.tsx
+++ website/src/pages/AboutPage.tsx
@@ -1,5 +1,7 @@
+import Footer  from '../components/Footer';
 function AboutPage() {
   return (
+    <div>
     <div className="container mx-auto px-4 py-10">
       <h1 className="text-4xl font-bold text-center mb-4">About Us</h1>
       <p className="max-w-2xl mx-auto text-center mb-6">
@@ -29,6 +31,8 @@ function AboutPage() {
           a difference, one item at a time.
         </p>
       </div>
+    </div>
+    <Footer/>
     </div>
   );
 }
