ad904fecb9812ea692ef3bc919ac683839b3c9cf
davidteather <34144122+davidteather@users.noreply.github.com>
2024-02-20 19:18:59 -0600

    autoformat

--- website/src/components/MessagePanel.tsx
+++ website/src/components/MessagePanel.tsx
@@ -40,23 +40,31 @@ const MessagePanel: React.FC = () => {
     user: string;
     messages: Message[];
   }
-  
+
   const groupMessages = (messages: Message[]): GroupedMessage[] => {
     const grouped: GroupedMessage[] = [];
     messages.forEach((msg: Message) => {
       const lastGroup = grouped[grouped.length - 1];
       const lastMessage = lastGroup?.messages[lastGroup.messages.length - 1];
       const timeDifference = lastMessage
-        ? (new Date(msg.sentAt).getTime() - new Date(lastMessage.sentAt).getTime()) / 60000
+        ? (new Date(msg.sentAt).getTime() -
+            new Date(lastMessage.sentAt).getTime()) /
+          60000
         : null;
-    
+
       //check if last message was sent within 5 minutes and by the same user
-      if (lastGroup && lastMessage && timeDifference !== null && timeDifference <= 5 && lastMessage.senderID === msg.senderID) {
+      if (
+        lastGroup &&
+        lastMessage &&
+        timeDifference !== null &&
+        timeDifference <= 5 &&
+        lastMessage.senderID === msg.senderID
+      ) {
         lastGroup.messages.push(msg);
       } else {
         grouped.push({
           user: msg.senderID,
-          messages: [msg]
+          messages: [msg],
         });
       }
     });
@@ -95,69 +103,105 @@ const MessagePanel: React.FC = () => {
   return (
     <div>
       <Header />
-    <div className="message-panel p-4 flex flex-col justify-between h-full bg-gray-100 min-h-screen">
-<div ref={messagesEndRef} className="messages overflow-y-auto flex flex-col gap-2">
-  {groupedMessages.length > 0 ? (
-    groupedMessages.map((group, groupIndex) => (
-      <div key={groupIndex} className="flex flex-col">
-        {group.messages.map((msg, index) => (
-          <div
-            key={index}
-            className={`chat ${msg.senderID === currentUserID ? "items-start" : "items-end"} flex flex-col`}
-          >
-            {index === 0 && (
-              // Adjust the flex container to align based on the sender
-              <div className={`flex ${msg.senderID === currentUserID ? "flex-row" : "flex-row-reverse"} items-center gap-2`}>
-                <div className="chat-image avatar">
-                  <ProfilePictureOrInitial user={msg.senderID === currentUserID ? user : otherUser} />
-                </div>
-                <div className={`chat-header text-sm ${msg.senderID === currentUserID ? "text-left" : "text-right"}`}>
-                  <span className="font-bold">{msg.senderID === currentUserID ? user?.name : otherUser?.name}</span>
-                  <time className="text-xs opacity-50 ml-2">
-                    {new Date(msg.sentAt).toLocaleTimeString()}
-                  </time>
-                </div>
+      <div className="message-panel p-4 flex flex-col justify-between h-full bg-gray-100 min-h-screen">
+        <div
+          ref={messagesEndRef}
+          className="messages overflow-y-auto flex flex-col gap-2"
+        >
+          {groupedMessages.length > 0 ? (
+            groupedMessages.map((group, groupIndex) => (
+              <div key={groupIndex} className="flex flex-col">
+                {group.messages.map((msg, index) => (
+                  <div
+                    key={index}
+                    className={`chat ${
+                      msg.senderID === currentUserID
+                        ? "items-start"
+                        : "items-end"
+                    } flex flex-col`}
+                  >
+                    {index === 0 && (
+                      // Adjust the flex container to align based on the sender
+                      <div
+                        className={`flex ${
+                          msg.senderID === currentUserID
+                            ? "flex-row"
+                            : "flex-row-reverse"
+                        } items-center gap-2`}
+                      >
+                        <div className="chat-image avatar">
+                          <ProfilePictureOrInitial
+                            user={
+                              msg.senderID === currentUserID ? user : otherUser
+                            }
+                          />
+                        </div>
+                        <div
+                          className={`chat-header text-sm ${
+                            msg.senderID === currentUserID
+                              ? "text-left"
+                              : "text-right"
+                          }`}
+                        >
+                          <span className="font-bold">
+                            {msg.senderID === currentUserID
+                              ? user?.name
+                              : otherUser?.name}
+                          </span>
+                          <time className="text-xs opacity-50 ml-2">
+                            {new Date(msg.sentAt).toLocaleTimeString()}
+                          </time>
+                        </div>
+                      </div>
+                    )}
+                    <div
+                      className={`chat-bubble shadow rounded-lg mt-1 ${
+                        msg.senderID === currentUserID
+                          ? "bg-blue-600 text-white p-3 self-start"
+                          : "bg-gray-200 text-gray-800 p-3 self-end"
+                      }`}
+                    >
+                      {msg.text}
+                    </div>
+                    {index === group.messages.length - 1 && (
+                      <div
+                        className={`chat-footer text-xs opacity-50 ${
+                          msg.senderID === currentUserID
+                            ? "self-start"
+                            : "self-end"
+                        }`}
+                      >
+                        {msg.senderID === currentUserID ? "Delivered" : ""}
+                      </div>
+                    )}
+                  </div>
+                ))}
               </div>
-            )}
-            <div
-              className={`chat-bubble shadow rounded-lg mt-1 ${msg.senderID === currentUserID ? "bg-blue-600 text-white p-3 self-start" : "bg-gray-200 text-gray-800 p-3 self-end"}`}
-            >
-              {msg.text}
+            ))
+          ) : (
+            <div className="flex flex-col items-center justify-center h-full">
+              <p className="text-lg text-gray-800 mb-4">No messages yet.</p>
             </div>
-            {index === group.messages.length - 1 && (
-              <div className={`chat-footer text-xs opacity-50 ${msg.senderID === currentUserID ? "self-start" : "self-end"}`}>
-                {msg.senderID === currentUserID ? "Delivered" : ""}
-              </div>
-            )}
-          </div>
-        ))}
+          )}
+        </div>
+        <div className="send-message-form mt-4 flex gap-2">
+          <textarea
+            className="textarea textarea-bordered flex-1 p-2"
+            value={newMessage}
+            onChange={(e) => setNewMessage(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && !e.shiftKey) {
+                e.preventDefault();
+                sendMessage();
+              }
+            }}
+            placeholder="Write a message..."
+          ></textarea>
+          <button className="btn btn-primary" onClick={sendMessage}>
+            Send
+          </button>
+        </div>
       </div>
-    ))
-  ) : (
-    <div className="flex flex-col items-center justify-center h-full">
-      <p className="text-lg text-gray-800 mb-4">No messages yet.</p>
-    </div>
-  )}
-</div>
-      <div className="send-message-form mt-4 flex gap-2">
-        <textarea
-          className="textarea textarea-bordered flex-1 p-2"
-          value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter' && !e.shiftKey) {
-              e.preventDefault();
-              sendMessage();
-            }
-          }
-        }
-          placeholder="Write a message..."
-        ></textarea>
-        <button className="btn btn-primary" onClick={sendMessage}>
-          Send
-        </button>
-      </div>
-    </div>
     </div>
   );
 };
--- website/src/pages/Items/ItemPage.tsx
+++ website/src/pages/Items/ItemPage.tsx
@@ -11,7 +11,11 @@ import RatingStars from "../../components/Ratings/StarRating";
 import RatingRow from "../../components/Ratings/RatingRow";
 import Ratings from "../../components/Ratings/Ratings";
 import { Rating } from "../../models/Rating";
-import { fetchItemRatings, fetchRatingById } from "../../services/RatingService";
+import {
+  fetchItemRatings,
+  fetchRatingById,
+} from "../../services/RatingService";
+import Header from "../../components/Header";
 
 const ItemPage = () => {
   let { itemID } = useParams();
@@ -52,9 +56,11 @@ const ItemPage = () => {
       })
       .catch((error) => setError("Failed to fetch item details."));
 
-    fetchItemRatings("items/" + itemID).then((fetchedRatings: Rating[]) => {
-      setRatings(fetchedRatings);
-    }).catch((error) => setError("Failed to fetch item ratings."));
+    fetchItemRatings("items/" + itemID)
+      .then((fetchedRatings: Rating[]) => {
+        setRatings(fetchedRatings);
+      })
+      .catch((error) => setError("Failed to fetch item ratings."));
   }, [itemID, user]);
 
   const handleSendMessage = async () => {
@@ -78,183 +84,195 @@ const ItemPage = () => {
   }
 
   return (
-    <div className="container mx-auto mt-4 mb-4">
-      {error && <div className="alert alert-error">{error}</div>}
-      <div className="flex flex-wrap lg:flex-nowrap">
-        <div
-          className="relative w-full lg:w-3/4"
-          style={{ overflow: "hidden" }}
-        >
-          <div className="carousel">
-            {item?.attachments?.map((attachment, index) => (
-              <div
-                id={`slide${index}`}
-                className="carousel-item w-full"
-                key={index}
-              >
-                <img
-                  src={attachment}
-                  alt={`Slide ${index}`}
-                  className="object-contain h-full w-full"
-                />
-              </div>
-            ))}
-          </div>
-          {/* Navigation Buttons */}
-          <div className="flex justify-center w-full gap-2">
-            <div className="flex justify-center w-full gap-2 overflow-x-auto">
+    <div>
+      <Header />
+      <div className="container mx-auto mt-4 mb-4">
+        {error && <div className="alert alert-error">{error}</div>}
+        <div className="flex flex-wrap lg:flex-nowrap">
+          <div
+            className="relative w-full lg:w-3/4"
+            style={{ overflow: "hidden" }}
+          >
+            <div className="carousel">
               {item?.attachments?.map((attachment, index) => (
-                <a
-                  href={`#slide${index}`}
-                  className="carousel-thumb"
+                <div
+                  id={`slide${index}`}
+                  className="carousel-item w-full"
                   key={index}
                 >
                   <img
                     src={attachment}
-                    alt={`Thumbnail ${index}`}
-                    className="object-cover h-16 w-16 rounded-lg shadow-lg"
+                    alt={`Slide ${index}`}
+                    className="object-contain h-full w-full"
                   />
-                </a>
+                </div>
               ))}
             </div>
-          </div>
-        </div>
-
-        {/* Item Details Section */}
-        <div className="w-full lg:w-1/4 pl-0 lg:pl-4 mt-4 lg:mt-0">
-          <div className="bg-white shadow rounded-lg overflow-hidden">
-            <div className="p-6">
-              <h2 className="text-2xl font-bold text-gray-900 mb-2">
-                {item?.title}
-              </h2>
-              <RatingRow key={avgRating} avgRating={avgRating} numRatings={numRatings} className="mb-2" />
-              {item?.createdAt && (
-                <p className="text-sm text-gray-500 mb-4">
-                  Posted on {new Date(item.createdAt).toLocaleDateString()} at{" "}
-                  {new Date(item.createdAt).toLocaleTimeString()}
-                </p>
-              )}
-
-              <p className="text-gray-700 mb-4">{item?.description}</p>
-
-              {owner && (
-                <div className="flex items-center gap-4 mb-4">
-                  <div className="shrink-0">
-                    <ProfilePictureOrInitial user={owner} />
-                  </div>
-                  <div>
-                    <h3 className="text-lg font-medium text-gray-900">
-                      {owner.name}
-                    </h3>
-                  </div>
-                </div>
-              )}
-
-              {Object.entries(item?.attributes || {}).some(
-                ([, value]) => value
-              ) && (
-                <div className="mb-4">
-                  <h4 className="text-md font-semibold text-gray-800 mb-2">
-                    Details
-                  </h4>
-                  <div className="border-t border-gray-200">
-                    {Object.entries(item?.attributes || {})
-                      .filter(([, value]) => value)
-                      .map(([key, value], idx) => (
-                        <div
-                          key={idx}
-                          className="py-2 flex justify-between items-center"
-                        >
-                          <span className="text-gray-600">
-                            {toTitleCaseWithSpaces(key)}
-                          </span>
-                          <span className="text-gray-900">
-                            {Array.isArray(value) ? value.join(", ") : value}
-                          </span>
-                        </div>
-                      ))}
-                  </div>
-                </div>
-              )}
-
-              {!isItemOwner && user && (
-                <button
-                  className="btn btn-primary w-full"
-                  onClick={() => setIsModalOpen(true)}
-                >
-                  Contact Owner
-                </button>
-              )}
-
-              {!isItemOwner && !user && (
-                <button
-                  className="btn btn-primary w-full"
-                  onClick={() => {
-                    nav("/login");
-                  }}
-                >
-                  Login to Contact Owner
-                </button>
-              )}
-
-              {isItemOwner && (
-                <>
-                  <button className="btn btn-secondary mr-2">Edit</button>
-                  <button
-                    onClick={() => {
-                      deleteItem(item?.id || "")
-                        .then(() => {
-                          // Redirect to the home page after successful deletion
-                          nav("/");
-                        })
-                        .catch((e) => {
-                          if (
-                            e.response &&
-                            e.response.data &&
-                            e.response.data.error
-                          ) {
-                            setError(e.response.data.error);
-                            return;
-                          }
-                          setError("An error occurred. Please try again.");
-                        });
-                    }}
-                    className="btn btn-error"
+            {/* Navigation Buttons */}
+            <div className="flex justify-center w-full gap-2">
+              <div className="flex justify-center w-full gap-2 overflow-x-auto">
+                {item?.attachments?.map((attachment, index) => (
+                  <a
+                    href={`#slide${index}`}
+                    className="carousel-thumb"
+                    key={index}
                   >
-                    Delete
+                    <img
+                      src={attachment}
+                      alt={`Thumbnail ${index}`}
+                      className="object-cover h-16 w-16 rounded-lg shadow-lg"
+                    />
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+
+          {/* Item Details Section */}
+          <div className="w-full lg:w-1/4 pl-0 lg:pl-4 mt-4 lg:mt-0">
+            <div className="bg-white shadow rounded-lg overflow-hidden">
+              <div className="p-6">
+                <h2 className="text-2xl font-bold text-gray-900 mb-2">
+                  {item?.title}
+                </h2>
+                <RatingRow
+                  key={avgRating}
+                  avgRating={avgRating}
+                  numRatings={numRatings}
+                  className="mb-2"
+                />
+                {item?.createdAt && (
+                  <p className="text-sm text-gray-500 mb-4">
+                    Posted on {new Date(item.createdAt).toLocaleDateString()} at{" "}
+                    {new Date(item.createdAt).toLocaleTimeString()}
+                  </p>
+                )}
+
+                <p className="text-gray-700 mb-4">{item?.description}</p>
+
+                {owner && (
+                  <div className="flex items-center gap-4 mb-4">
+                    <div className="shrink-0">
+                      <ProfilePictureOrInitial user={owner} />
+                    </div>
+                    <div>
+                      <h3 className="text-lg font-medium text-gray-900">
+                        {owner.name}
+                      </h3>
+                    </div>
+                  </div>
+                )}
+
+                {Object.entries(item?.attributes || {}).some(
+                  ([, value]) => value
+                ) && (
+                  <div className="mb-4">
+                    <h4 className="text-md font-semibold text-gray-800 mb-2">
+                      Details
+                    </h4>
+                    <div className="border-t border-gray-200">
+                      {Object.entries(item?.attributes || {})
+                        .filter(([, value]) => value)
+                        .map(([key, value], idx) => (
+                          <div
+                            key={idx}
+                            className="py-2 flex justify-between items-center"
+                          >
+                            <span className="text-gray-600">
+                              {toTitleCaseWithSpaces(key)}
+                            </span>
+                            <span className="text-gray-900">
+                              {Array.isArray(value) ? value.join(", ") : value}
+                            </span>
+                          </div>
+                        ))}
+                    </div>
+                  </div>
+                )}
+
+                {!isItemOwner && user && (
+                  <button
+                    className="btn btn-primary w-full"
+                    onClick={() => setIsModalOpen(true)}
+                  >
+                    Contact Owner
                   </button>
-                </>
-              )}
+                )}
+
+                {!isItemOwner && !user && (
+                  <button
+                    className="btn btn-primary w-full"
+                    onClick={() => {
+                      nav("/login");
+                    }}
+                  >
+                    Login to Contact Owner
+                  </button>
+                )}
+
+                {isItemOwner && (
+                  <>
+                    <button className="btn btn-secondary mr-2">Edit</button>
+                    <button
+                      onClick={() => {
+                        deleteItem(item?.id || "")
+                          .then(() => {
+                            // Redirect to the home page after successful deletion
+                            nav("/");
+                          })
+                          .catch((e) => {
+                            if (
+                              e.response &&
+                              e.response.data &&
+                              e.response.data.error
+                            ) {
+                              setError(e.response.data.error);
+                              return;
+                            }
+                            setError("An error occurred. Please try again.");
+                          });
+                      }}
+                      className="btn btn-error"
+                    >
+                      Delete
+                    </button>
+                  </>
+                )}
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      {/* Modal for sending a message */}
-      {isModalOpen && (
-        <div className="modal modal-open">
-          <div className="modal-box">
-            <h3 className="font-bold text-lg">Contact Owner</h3>
-            <textarea
-              className="textarea textarea-bordered w-full mt-4"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            />
-            <div className="modal-action">
-              <button className="btn btn-primary" onClick={handleSendMessage}>
-                Send Message
-              </button>
-              <button className="btn" onClick={() => setIsModalOpen(false)}>
-                Cancel
-              </button>
+        {/* Modal for sending a message */}
+        {isModalOpen && (
+          <div className="modal modal-open">
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">Contact Owner</h3>
+              <textarea
+                className="textarea textarea-bordered w-full mt-4"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              <div className="modal-action">
+                <button className="btn btn-primary" onClick={handleSendMessage}>
+                  Send Message
+                </button>
+                <button className="btn" onClick={() => setIsModalOpen(false)}>
+                  Cancel
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
 
-      {/* Ratings */}
-      <div className="mt-8">
-      <Ratings ratings={ratings || []} recipientID={item?.id} recipientIsItem={true} />
+        {/* Ratings */}
+        <div className="mt-8">
+          <Ratings
+            ratings={ratings || []}
+            recipientID={item?.id}
+            recipientIsItem={true}
+          />
+        </div>
       </div>
     </div>
   );
