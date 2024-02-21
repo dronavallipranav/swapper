0e859938255a5d76b93c77527d25206a0d744d58
davidteather <34144122+davidteather@users.noreply.github.com>
2024-02-20 19:45:40 -0600

    rating on home page

--- website/src/pages/HomePage.tsx
+++ website/src/pages/HomePage.tsx
@@ -1,23 +1,23 @@
 import React, { useEffect, useRef, useState, useCallback } from "react";
-import { Item, Attributes} from "../models/Item";
+import { Item, Attributes } from "../models/Item";
 import { fetchAllItems } from "../services/ItemService";
 import { Location } from "../services/LocationService";
 import AttributeSelector from "../components/AttributeSelect";
 import { useNavigate } from "react-router-dom";
-import { debounce } from 'lodash';
+import { debounce, fill } from "lodash";
 import HomeHeader from "../components/HomeHeader";
+import { filledStar } from "../components/Ratings/StarRating";
 
 const HomePage = () => {
   const [items, setItems] = useState<Item[] | null>(null);
   const [selectedCategory, setSelectedCategory] = useState<string>("All");
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [location, setLocation] = useState<Location | undefined>(undefined);
   const [selectedRadius, setSelectedRadius] = useState<number>(10);
   const [isFilterVisible, setIsFilterVisible] = useState<boolean>(false);
   const attributeSelectorRef = useRef(null);
   const [attributes, setAttributes] = useState<Record<string, string[]>>({});
   const nav = useNavigate();
-
 
   const updateAttributes = (newAttributes: Record<string, string[]>) => {
     setAttributes(newAttributes);
@@ -36,29 +36,31 @@ const HomePage = () => {
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
-  const fetchItemsDebounced = useCallback(debounce((query) => {
-    
-    fetchAllItems({
-      ...query,
-      latitude: location?.latitude || 43.0731,
-      longitude: location?.longitude || -89.4012,
-      radius: selectedRadius,
-      categories: selectedCategory !== "All" ? [selectedCategory] : [],
-      status: "available",
-      limit: 500,
-      skip: 0,
-      sort: "title",
-      order: "asc",
-      search: search,
-      attributes: attributes
-    })
-    .then(fetchedItems => {
-      setItems(fetchedItems);
-    })
-    .catch(error => {
-      console.error(error);
-    });
-  }, 300), [search, selectedCategory, selectedRadius, location, attributes]); //dependencies for the debounced function
+  const fetchItemsDebounced = useCallback(
+    debounce((query) => {
+      fetchAllItems({
+        ...query,
+        latitude: location?.latitude || 43.0731,
+        longitude: location?.longitude || -89.4012,
+        radius: selectedRadius,
+        categories: selectedCategory !== "All" ? [selectedCategory] : [],
+        status: "available",
+        limit: 500,
+        skip: 0,
+        sort: "title",
+        order: "asc",
+        search: search,
+        attributes: attributes,
+      })
+        .then((fetchedItems) => {
+          setItems(fetchedItems);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }, 300),
+    [search, selectedCategory, selectedRadius, location, attributes]
+  ); //dependencies for the debounced function
 
   useEffect(() => {
     fetchItemsDebounced({ search });
@@ -76,76 +78,86 @@ const HomePage = () => {
 
   return (
     <div>
-    <HomeHeader 
-                    search ={search} 
-                    setSearch={setSearch}
-                    location={location}
-                    setLocation={setLocation}
-                    selectedRadius={selectedRadius}
-                    setSelectedRadius={setSelectedRadius}
-                    isFilterVisible={isFilterVisible}
-                    setIsFilterVisible={setIsFilterVisible}
-                    selectedCategory={selectedCategory}
-                    setSelectedCategory={setSelectedCategory}
-                    />
-    <div className="container mx-auto px-4 py-10">
- 
-      <div
-        className={`fixed inset-0 z-40 transform ${
-          isFilterVisible ? "translate-x-0" : "-translate-x-full"
-        } transition-transform ease-in-out duration-300 ${
-          isFilterVisible ? "w-full md:w-64" : "w-0"
-        } bg-white shadow-xl overflow-hidden`}
-        ref={attributeSelectorRef}
-      >
-        <div className="flex justify-between items-center p-4 border-b">
-          <h2 className="text-lg font-semibold">Filters</h2>
-          <button
-            onClick={() => setIsFilterVisible(false)}
-            className="btn btn-square btn-sm"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="currentColor"
-              className="w-6 h-6"
+      <HomeHeader
+        search={search}
+        setSearch={setSearch}
+        location={location}
+        setLocation={setLocation}
+        selectedRadius={selectedRadius}
+        setSelectedRadius={setSelectedRadius}
+        isFilterVisible={isFilterVisible}
+        setIsFilterVisible={setIsFilterVisible}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
+      <div className="container mx-auto px-4 py-10">
+        <div
+          className={`fixed inset-0 z-40 transform ${
+            isFilterVisible ? "translate-x-0" : "-translate-x-full"
+          } transition-transform ease-in-out duration-300 ${
+            isFilterVisible ? "w-full md:w-64" : "w-0"
+          } bg-white shadow-xl overflow-hidden`}
+          ref={attributeSelectorRef}
+        >
+          <div className="flex justify-between items-center p-4 border-b">
+            <h2 className="text-lg font-semibold">Filters</h2>
+            <button
+              onClick={() => setIsFilterVisible(false)}
+              className="btn btn-square btn-sm"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
-        </div>
-  
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+
           <AttributeSelector onAttributesChange={updateAttributes} />
-          
-      </div>
+        </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 xl:grid-cols-4 justify-items-center">
-        {items &&
-          items.map((item) => (
-            <div
-              key={item.id}
-              className="card card-compact w-full md:w-75 bg-base-100 shadow-xl hover:scale-110 cursor-pointer transform transition-transform"
-              onClick={() => {
-                nav(`/${item.id}`)
-              }}
-            >
-              {item.attachments && item.attachments[0] && (
-                <figure>
-                  <img src={item.attachments[0]} alt="Item" />
-                </figure>
-              )}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 xl:grid-cols-4 justify-items-center">
+          {items &&
+            items.map((item) => (
+              <div
+                key={item.id}
+                className="card card-compact w-full md:w-75 bg-base-100 shadow-xl hover:scale-110 cursor-pointer transform transition-transform"
+                onClick={() => {
+                  nav(`/${item.id}`);
+                }}
+              >
+                {item.attachments && item.attachments[0] && (
+                  <figure>
+                    <img src={item.attachments[0]} alt="Item" />
+                  </figure>
+                )}
 
-              <div className="card-body mt-[-8px]">
-                <div>
-                  <h2 className="text-2xl font-bold text-gray-900">
-                    {item.title}
-                  </h2>
+                <div className="card-body mt-[-8px]">
+                  <div className="flex justify-between items-center">
+                    <h2 className="text-2xl font-bold text-gray-900">
+                      {item.title}
+                    </h2>
+
+                    <div className="flex items-center">
+                    <span className="w-3 h-3 inline-block mr-1">{filledStar}</span>{" "}
+                      <span>
+                        {item.avgRating
+                          ? (item.avgRating / 2).toFixed(1)
+                          : "0.0"}
+                      </span>
+                      
+                    </div>
+                  </div>
+
                   {item?.createdAt && (
                     <div className="flex items-center text-gray-400 text-xs">
                       <span className="ml-2">
@@ -169,12 +181,11 @@ const HomePage = () => {
                   <p className="text-gray-700 mt-2">{item.description}</p>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+        </div>
       </div>
     </div>
-    </div>
   );
-}
+};
 
 export default HomePage;
